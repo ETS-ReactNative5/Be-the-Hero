daf12957381081116fe90720415a66bd84e62649
hemerson-git <hemersonsimoni@gmail.com>
2020-03-29 00:07:56 -0300

    frontend application

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,22 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiLogIn } from 'react-icons/fi'
 
 import './styles.css';
 
 import logoImg from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import api from '../../services/api';
 
 export default function Logon() {
+  const [id, setId] = useState('');
+  const history = useHistory();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    try {
+      const response = await api.post('session', {id}); 
+
+      localStorage.setItem('ongID', id);
+      localStorage.setItem('ongName', response.data.name);
+
+      history.push('/profile')
+    } catch (err) {
+      alert('Falha no login, tente novamente');
+    }
+  }
+
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logoImg} alt="Be The Hero"/>
 
-        <form action="">
+        <form onSubmit={handleLogin}>
           <h1>Faça seu logon</h1>
 
-          <input type="text" placeholder="Sua ID"/>
+          <input type="text" placeholder="Sua ID"
+            value={id}
+            onChange={e => setId(e.target.value)}
+          />
+
           <button type="submit" className="button">Entrar</button>
 
           <Link to="/register" className="back-link">
